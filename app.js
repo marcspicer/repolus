f2dbaa5eb3158f77c940badb96d9291334b5df53
unknown <marc@marcspicer.co>
2018-06-06 16:17:38 -0400

    remove /dist path from app.js

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.use(express.static(__dirname + '/dist'));
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
-  app.get('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname, 'index.html'));
-  });
+  // app.get('*', function (request, response) {
+  //   response.sendFile(path.resolve(__dirname, 'index.html'));
+  // });
 // end serve the angular index file work
 
 app.use(cors());
